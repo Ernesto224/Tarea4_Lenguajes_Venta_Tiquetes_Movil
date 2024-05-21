99a6e7b826797844c91263ad6f2f40f7916e4c34
Jesner Eliecer Melgara Murillo <jesner.melgara@ucr.ac.cr>
2024-05-21 01:53:28 -0600

    API ACTUALIZADA

---
+++ VentaConciertoAPI/Venta.BW/CU/GestionarVentaBW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venta.BW.Interfaces.BW;
+
+namespace Venta.BW.CU
+{
+    public class GestionarVentaBW: IGestionarVentaBW
+    {
+        private readonly IGestionarVentaBW gestionarVentaBW;
+        public GestionarVentaBW(IGestionarVentaBW _gestionarVentaBW)
+        {
+            this.gestionarVentaBW = _gestionarVentaBW;
+        }
+    }
+}
