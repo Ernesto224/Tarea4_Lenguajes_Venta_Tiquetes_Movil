7c152e3b71c88d33d8e05d9b83911dde3f009c46
Jesner Eliecer Melgara Murillo <jesner.melgara@ucr.ac.cr>
2024-05-21 01:11:48 -0600

    API ACTUALIZADA

---
+++ VentaBoletosAPI/Controllers/VentanConciertoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Venta.BW.Interfaces.BW;
+
+namespace VentaBoletosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentanConciertoController : ControllerBase
+    {
+        private readonly IGestionarVentaBW gestionarVentaBW;
+        public VentanConciertoController(IGestionarVentaBW _gestionarVentaBW)
+        {
+            this.gestionarVentaBW = _gestionarVentaBW;  
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
